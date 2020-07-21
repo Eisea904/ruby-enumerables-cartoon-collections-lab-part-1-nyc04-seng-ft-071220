ea24aba435527bbc59e7758abdddfd6c79461295
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-21 16:14:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   count = 1
   array.each do |dwarf|
-    puts "#{count}. #{dward}"
+    puts "#{count}. #{dwarf}"
+    count += 1
   end 
 end
